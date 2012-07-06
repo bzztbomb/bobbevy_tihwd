721a68ef920ba931fb2754cbb68c01cd1ca39bc3
bzztbomb <skinny@knowhere.net>
2012-07-06 00:49:49 -0700

    Cleanup interface a little

--- control/bobbevy.js
+++ control/bobbevy.js
@@ -1,29 +1,55 @@
-loadedInterfaceName = "simple";
+loadedInterfaceName = "bobbevy";
 interfaceOrientation = "portrait";
 
 pages = [ [
-{
-    "name" : "lightButton",
-    "type" : "Button",
-    "mode": "momentary",
-    "x" : 0, "y" : 0,
-    "width" : .1, "height" : .1,
-    "label" : "Disable light",
-    "ontouchstart" : "oscManager.sendOSC(\"/bobbevy/key\", \"s\", \"L\");"
-},
-{
-"name": "refreshButton",
-"type": "Button",
-"x": 0,
-"y": .90,
-"width": .15,
-"height": .10,
-"mode": "momentary",
-"color": "#000000",
-"stroke": "#aaaaaa",
-"ontouchstart": "interfaceManager.refreshInterface()",
-    "label":"Refresh"
-}
+    {
+        "name" : "lightButton",
+        "type" : "Button",
+        "mode": "momentary",
+        "x" : 0, "y" : 0,
+        "width" : .1, "height" : .1,
+        "label" : "Toggle light",
+        "ontouchstart" : "oscManager.sendOSC(\"/bobbevy/key\", \"s\", \"L\");",
+			"isLocal": true,
+    },
+    {
+        "name" : "treeButton",
+        "type" : "Button",
+        "mode": "momentary",
+        "x" : .1, "y" : 0,
+        "width" : .1, "height" : .1,
+        "label" : "Toggle trees",
+        "ontouchstart" : "oscManager.sendOSC(\"/bobbevy/key\", \"s\", \"T\");",
+			"isLocal": true,
+    },
+    {
+        "name": "refreshButton",
+        "type": "Button",
+        "x": 0,
+        "y": .90,
+        "width": .15,
+        "height": .10,
+        "mode": "momentary",
+        "color": "#000000",
+        "stroke": "#aaaaaa",
+        "ontouchstart": "interfaceManager.refreshInterface()",
+        "label":"Refresh",
+			"isLocal": true,
+    },
+	 {
+	     "name": "tabButton",
+	     "type": "Button",
+	     "x": .15,
+	     "y": .80,
+	     "width": .145,
+	     "height": .10,
+	     "mode":"toggle",
+	     "colors": ["#000", "#444", "#aaa"],
+	     "ontouchstart": "control.showToolbar();",
+	     "label": "menu",
+			"isLocal": true,
+	 },
+
 ],
 
 ];
